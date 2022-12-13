80fe27a567f7e704001d85f98599c60588d86808
Elliott Schwarzman <nschwrzm@memphis.edu>
2022-12-13 11:42:32 -0600

    Lifting State Up; Board now maintains game state using squares[9] so disabled Square constructor and modified render to use props passed down, defined handleClick()

--- src/index.js
+++ src/index.js
@@ -3,33 +3,61 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 class Square extends React.Component {
+  // constructor disabled because Square no longer keeps track of the game's state
   // constructor to initialize state
-  constructor(props) {
-    super(props); // all react component classes w/ constructor: must call ```super(props);``` first
-    this.state = {  // object where value: null
-      value: null,
-    };
-  }
+  // constructor(props) {
+  //   super(props); // all react component classes w/ constructor: must call ```super(props);``` first
+  //   this.state = {  // object where value: null
+  //     value: null,
+  //   };
+  // }
 
   render() { // {this.props.value} shows the value passed by prop[erties] called value. Pretty self-explanatory, just passing {value} into <button /> tags
     return (
       <button
         className="square"
-        onClick={() => this.setState({value: 'X'})}
+        onClick={() => this.props.onClick()}
       > {/* syntax equivalent onClick={function() { foobar }}
             new line for readability
             note: don't comment inside tag braces
+            """"""
+            was this.setState({value: 'X'}) -> now this.props.onClick(), see onClick prop passed by Board.renderSquare()
         */}
-        {this.state.value} {/* display state */}
+        {this.props.value} {/* was this.state.value, but now parent (Board) tracks each square's state which is passed down through props */}
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    }; // note: "lifting state" into a parent to share info among children
+    /* init such that
+      squares = [
+        null, null, null,
+        null, null, null,
+        null, null, null,
+      ]
+      */
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({squares: squares});
+  }
+
   renderSquare(i) {
-    return <Square value={i} />; // pass prop called value to the Square
-    // return <Square />; // original
+    return ( // pass the ith state in squares array 'down' (parent Board->child Square) to this Square's value
+      <Square
+        value={this.state.squares[i]} // value (1/2) prop passed down from Board to Square
+        onClick={() => this.handleClick(i)} // onClick (2/2) prop, defines function called on click event
+      />
+      // note: follow React props naming convention on[Event] and handle[Event], even though they could be nAmEdaNYtHiNG
+    );
   }
 
   render() {
